bf9bfcb948eb6d74698c4306615ddd6edc104325
Diego Perez <diego.fernando.perez.luna@gmail.com>
2025-07-29 00:27:53 -0400

    Add: chat square & scrollbar

--- src/components/input-component/InputComponent.tsx
+++ src/components/input-component/InputComponent.tsx
@@ -1,7 +1,9 @@
 interface InputComponentProps {
   placeholder?: string;
   alt?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
 }
-export const InputComponent = ({ placeholder, alt }: InputComponentProps) => {
-  return <input placeholder={placeholder} alt={alt} className="border border-gray-300 p-2 rounded-md w-full" />;
+export const InputComponent = ({ placeholder, alt, onChange, value }: InputComponentProps) => {
+  return <input placeholder={placeholder} alt={alt} className="border border-gray-300 p-2 rounded-md w-full" onChange={onChange} value={value} />;
 };
