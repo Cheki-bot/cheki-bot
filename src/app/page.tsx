d6e94d32e96ae061d850a5807540ece080ff9842
Diego Perez <diego.fernando.perez.luna@gmail.com>
2025-07-28 23:11:52 -0400

    Add: button component finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import { ButtonComponent } from "@/components";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Bienvenido a Cheki Bot</h1>
       <p className="mt-4">
-        Tu asistente de IA para todo lo relacionado con el automóvil.
+        Tu asistente de IA para todo lo relacionado con el proceso electoral.
       </p>
+      <div className="w-full">
+        <ButtonComponent>Consultar</ButtonComponent>
+      </div>
     </main>
   );
 }
