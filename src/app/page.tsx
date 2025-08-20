d0ad3a68af41dc11ac53855464c72eeca149a734
Diegopl <diego.fernando.perez.luna@gmail.com>
2025-08-20 16:45:35 -0400

    Add: checki bot correction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 
     return (
         <div
-        className="flex flex-col p-2 md:p-0 bg-neutral-900 text-white"
+            className="flex flex-col p-2 md:p-0 bg-neutral-900 text-white"
             style={{ height: 'calc(var(--vh, 1vh) * 100)' }}
         >
             {openModal && (
@@ -105,7 +105,7 @@ function ChatHeader({ onNewChat }: { onNewChat: () => void }) {
     return (
         <header className="flex md:px-6 md:py-4 px-4 py-2 h-fit justify-between items-center w-full top-0 border-b border-neutral-700 shadow-lime-300/10 bg-neutral-900 ">
             <span className="md:w-full w-2/3">
-                <h1 className="text-xl font-semibold">Cheki Bot</h1>
+                <h1 className="text-xl font-semibold">Checki Bot</h1>
                 <p className="md:text-sm text-xs text-neutral-400">
                     Tu asistente de IA para todo lo relacionado con el proceso
                     electoral.
@@ -159,13 +159,13 @@ function WelcomeMessage() {
                 alt="Logo chatbot Chekibot"
             />
             <h1 className="text-2xl font-bold text-center md:text-4xl text-[#68BEB4]">
-                Bienvenido a Cheki Bot
+                Bienvenido a Checki Bot
             </h1>
             <div className="flex mt-4 items-center justify-center w-full h-full gap-2">
                 <p className="text-center flex flex-col md:flex-row items-center gap-2 text-neutral-300">
                     Tu asistente de IA para todo lo relacionado con el proceso
                     electoral creado por
-                    <span className='flex items-center'>
+                    <span className="flex items-center">
                         <ChequeaBoliviaLogo className="w-7 h-7 aspect-square" />
                         <strong className="text-xl">
                             <span className="text-white">Chequea</span>{' '}
