a7af7a29247ef9a46b3c9c356c65242bdc142f8d
Diego <diego.fernando.perez.luna@gmail.com>
2026-02-20 10:28:20 -0400

    Test analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,28 +24,22 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${baloo2.variable} antialiased`}>
-                {GA_ID && (
-                    <>
-                       <Script
-                            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-                            strategy="afterInteractive"
-                        />
-                        {/* Inicializa la configuración */}
-                        <Script id="gtag-init" strategy="afterInteractive">
-                            {`
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', '${GA_ID}', {
-                                    page_path: window.location.pathname,
-                                });
-                            `}
-                        </Script>
-                    </>
-                )}
                 {children}
+                {/* Global Site Tag (gtag.js) - Google Analytics */}
+                <Script
+                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_ID}');
+          `}
+                </Script>
             </body>
         </html>
     );
 }
-
